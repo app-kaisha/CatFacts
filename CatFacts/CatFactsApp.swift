0b98824f3f1d6960a3e3b9f0181726eb0bdf0b33
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-16 16:34:27 +0000

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,18 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by app-kaihatsusha on 16/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
