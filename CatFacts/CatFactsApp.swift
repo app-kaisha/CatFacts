af42c657fc8d135a4f7d39af223a2295c3a40a93
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-16 16:38:13 +0000

    Initial assets loadeda and setup

--- CatFacts/CatFactsApp.swift
+++ CatFacts/CatFactsApp.swift
@@ -5,6 +5,7 @@
 //  Created by app-kaihatsusha on 16/01/2026.
 //  Copyright © 2026 app-kaihatsusha. All rights reserved.
 //
+// https://catfact.ninja/breeds https://catfact.ninja/facts
 
 import SwiftUI
 
